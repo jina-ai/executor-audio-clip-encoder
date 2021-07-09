0e7713a96f6398427ac2f56293eebdf193bd3a47
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-07-09 15:52:22 +0100

    feat: update Dockerfile, manifest and setup files

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ __license__ = "Apache-2.0"
 import setuptools
 
 setuptools.setup(
-    name='jinahub-MY-DUMMY-EXECUTOR',
+    name='jinahub-audioclip-encoder',
     version='1',
     author='Jina Dev Team',
     author_email='dev-team@jina.ai',
-    description='This is my dummy executor',
-    url='https://github.com/jina-ai/EXECUTOR_REPO_NAME',
+    description='Executor that encodes audio data with AudioCLIP',
+    url='https://github.com/jina-ai/executor-audio-clip-encoder',
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
     ],
-    py_modules=['jinahub.SUB_PACKAGE_NAME.MODULE_NAME'],
-    package_dir={'jinahub.SUB_PACKAGE_NAME': '.'},
+    py_modules=['jinahub.encoder.audioclip'],
+    package_dir={'jinahub.encoder': '.'},
     install_requires=open('requirements.txt').readlines(),
     python_requires='>=3.7',
 )
