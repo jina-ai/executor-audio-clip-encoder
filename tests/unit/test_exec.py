338c76bdd8ce4bdda46581c99c8f92b88e26c691
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-07-15 16:37:37 +0100

    test: add unit and integration tests

--- tests/unit/test_exec.py
+++ tests/unit/test_exec.py
@@ -1,9 +1,24 @@
 __copyright__ = "Copyright (c) 2020-2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-from jina.executors import BaseExecutor
+import os
+import librosa
+
+from jina import Executor, Document, DocumentArray
+
+from audio_clip_encoder import AudioCLIPEncoder
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_exec():
-    ex = BaseExecutor.load_config('../../config.yml')
-    assert ex._dim == 128
+def test_load():
+    encoder = Executor.load_config(os.path.join(cur_dir, '../../config.yml'))
+    assert encoder.model_path.endswith('AudioCLIP-Full-Training.pt')
+
+
+def test_embedding_dimension():
+    x_audio, sample_rate = librosa.load(os.path.join(cur_dir, '../data/sample.wav'))
+    doc = DocumentArray([Document(blob=x_audio)])
+    model = AudioCLIPEncoder()
+    model.encode(doc, parameters={})
+    assert doc[0].embedding.shape == (1, 1024)
