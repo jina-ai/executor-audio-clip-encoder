de0222d6c493d0f6ba5a7189e5344ecdc45063fb
Sami Jaghouar <sami.jaghouar@jina.ai>
2022-03-09 17:47:31 +0100

    fix: add evil batchnorm

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -1,3 +1,5 @@
+import copy
+
 from audio_clip.model import AudioCLIP
 from audio_clip_v2.model import AudioCLIP as AudioCLIPv2
 import torch
@@ -5,7 +7,9 @@ import torch
 import pytest
 import os
 import librosa
+from audio_clip_v2.model.esresnet.fbsp import _ESResNetFBSP
 
+cur_dir = os.path.dirname(os.path.abspath(__file__))
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -38,12 +42,24 @@ def text_input():
     return ['Hello', 'beautiful!']
 
 
+def test_same_output_audio_0(new_model, audio_input):
+    old_model = AudioCLIPv2(
+        pretrained=os.path.join(cur_dir, '../../.cache/AudioCLIP-Full-Training.pt')
+    ).to('cpu')
+
+    with torch.inference_mode():
+        x_audio = torch.tensor([audio_input, audio_input])
+        embedding_v2 = new_model.encode_audio(audio=x_audio)[0]
+        embedding_v1 = old_model.encode_audio(audio=x_audio)[0]
+        assert torch.allclose(embedding_v1, embedding_v2)
+
+
 @torch.inference_mode()
 def test_same_output_audio(old_model, new_model, audio_input):
     x_audio = torch.tensor([audio_input, audio_input])
     embedding_v1 = old_model.encode_audio(audio=x_audio)[0]
     embedding_v2 = new_model.encode_audio(audio=x_audio)[0]
-    assert bool((embedding_v1 == embedding_v2).all())
+    assert torch.allclose(embedding_v1, embedding_v2)
 
 
 @torch.inference_mode()
@@ -52,12 +68,27 @@ def test_batchsize_one_audio(new_model, audio_input):
     x_audio_single = torch.tensor([audio_input])
     embedding_single = new_model.encode_audio(audio=x_audio_single)[0]
     embedding_double = new_model.encode_audio(audio=x_audio_double)[0]
-    assert ((embedding_single[0] - embedding_double[0]) ** 2) < 0.001
+    assert torch.allclose(embedding_single[0], embedding_double[0])
+
+
+def test_same_weights(old_model, new_model, audio_input):
+
+    with torch.no_grad():
+        for param1, param2 in zip(new_model.parameters(), old_model.parameters()):
+            assert param1.data.ne(param2.data).sum() == 0
+
 
 
 @torch.inference_mode()
 def test_same_output_text(old_model, new_model, text_input):
     embedding_v1 = old_model.encode_text(text_input)[0]
     embedding_v2 = new_model.encode_text(text_input)[0]
-    assert bool((embedding_v1 == embedding_v2).all())
+    assert torch.equal(embedding_v1, embedding_v2)
 
+
+@torch.inference_mode()
+def test_same_output_text(old_model, new_model, text_input):
+    model = _ESResNetFBSP()
+    model_eval = _ESResNetFBSP().eval()
+
+    assert torch.equal(embedding_v1, embedding_v2)
